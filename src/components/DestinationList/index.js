925d3ebcaa1936049d0ecdef805efed4899ea265
nandinivenkateshan <nandinivsa@gmail.com>
2020-08-30 12:23:52 +0530

    Refactred

--- src/components/DestinationList/index.js
+++ src/components/DestinationList/index.js
@@ -3,57 +3,88 @@ import Select from 'react-select'
 import '../../assets/css/style.css'
 
 function DestinationList ({ data, onUpdateState, onSubmitVal }) {
+  const MAX_DESTINATION = 4
+  const resetCurDestVehicles = (vehicles, destination) => {
+    let vehicleName = ''
+    vehicles.forEach(elem => {
+      if (elem.checked) {
+        document.getElementById(`${elem.name + destination}`).checked = false
+        elem.checked = false
+        elem.total_no += 1
+        vehicleName = elem.name
+      }
+    })
+    return vehicleName
+  }
+
+  const updateVehiclesStatus = (vehicles, planetDistance) => {
+    vehicles.forEach((elem) => {
+      !elem.total_no || planetDistance > elem.max_distance
+        ? (elem.isDisable = true)
+        : (elem.isDisable = false)
+    })
+  }
+
+  const updatePlanetsStatus = (planets, planetName) => {
+    return planets.map(planet => {
+      planet.name === planetName
+        ? (planet.isSelected = true)
+        : (planet.isSelected = false)
+      return planet
+    })
+  }
+
+  const setNextDestVehicles = (item, vehicleName) => {
+    item.vehicles.forEach(elem => {
+      if (vehicleName === elem.name) {
+        elem.total_no += 1
+        item.isSelectBtn = false
+      }
+    })
+  }
+
+  const stringify = val => JSON.parse(JSON.stringify(val))
+
+  const setPlanetsToNextDest = planets => stringify(planets.filter(item => !item.isSelected))
+
+  const calcSpeedAndVehiclesLeft = (item, vehicleName, destination, planetDistance) => {
+    item.vehicles.forEach(elem => {
+      if (elem.name === vehicleName && !elem.count) {
+        elem.total_no -= 1
+        elem.count += 1
+        if (item.destination === destination) {
+          const distance = planetDistance / elem.speed
+          item.time = distance
+        }
+      }
+      if (elem.name !== vehicleName && elem.count) {
+        elem.total_no += 1
+        elem.count -= 1
+      }
+    })
+  }
+
   const handlePlanet = (value, destination, planetIndex) => {
     const planetName = value.name
-    let updatedPlanets
+    let updatedPlanetsStatus
     let vehicleName = ''
     const updatedState = data.map((item, index) => {
       if (index === planetIndex) {
-        if (item.showVehicles) {
-          item.vehicles.forEach(elem => {
-            if (elem.checked) {
-              // console.log('ele',elem)
-              document.getElementById(`${elem.name + destination}`).checked = false
-              elem.checked = false
-              elem.total_no += 1
-              vehicleName = elem.name
-            }
-          })
-        }
         item.showVehicles = true
         item.time = 0
-        // disable the vehicle based on the speed and distance
-        item.vehicles.forEach((elem) => {
-          !elem.total_no || value.distance > elem.max_distance
-            ? (elem.isDisable = true)
-            : (elem.isDisable = false)
-        })
-        // Don't show the selected planet in the next destination
-        updatedPlanets = item.planets.map((planet) => {
-          planet.name === planetName
-            ? (planet.isSelected = true)
-            : (planet.isSelected = false)
-          return planet
-        })
-        item.planets = JSON.parse(JSON.stringify(updatedPlanets))
+        if (item.showVehicles) vehicleName = resetCurDestVehicles(item.vehicles, destination)
+        updateVehiclesStatus(item.vehicles, value.distance)
+        updatedPlanetsStatus = updatePlanetsStatus(item.planets, planetName)
+        item.planets = stringify(updatedPlanetsStatus)
       }
+      // Reset count value
       if (item.destination >= destination) {
-        item.vehicles.forEach((item) => {
+        item.vehicles.forEach(item => {
           item.count = 0
         })
       }
-      if (item.destination > destination) {
-        item.vehicles.forEach(elem => {
-          if (vehicleName === elem.name) {
-            elem.total_no += 1
-            item.isSelectBtn = false
-          }
-        })
-      }
-      if (item.destination !== destination && item.destination > planetIndex) {
-        const deSelectedPlanets = updatedPlanets.filter((item) => !item.isSelected)
-        item.planets = JSON.parse(JSON.stringify(deSelectedPlanets))
-      }
+      if (item.destination > destination) setNextDestVehicles(item, vehicleName)
+      if (item.destination !== destination && item.destination > planetIndex) item.planets = setPlanetsToNextDest(updatedPlanetsStatus)
       if (item.destination < destination) {
         item.isSelectBtn = false
         item.vehicles.forEach(elem => {
@@ -69,38 +100,20 @@ function DestinationList ({ data, onUpdateState, onSubmitVal }) {
     const vehicleName = e.target.value
     const planetDistance = data[planetIndex].planets.filter((item) => item.isSelected)[0].distance
     const updatedState = data.map(item => {
-      // disable the select options
-      if (item.destination === destination + 1 && destination <= 4) {
+      // Activate next select Btn the select options
+      if (item.destination === destination + 1 && destination <= MAX_DESTINATION) {
         item.isSelectBtn = true
       }
       // Activate the submit button once destination4's vehicle selected
-      if (destination === 4) {
-        onSubmitVal(true)
-      }
+      if (destination === 4) onSubmitVal(true)
       if (destination === item.destination) {
-        item.vehicles.forEach((ele) => {
+        item.vehicles.forEach(ele => {
           ele.name === vehicleName
             ? (ele.checked = true)
             : (ele.checked = false)
         })
       }
-      // Calaculate the time and no.of total vehicles left
-      if (item.destination >= destination) {
-        item.vehicles.forEach(elem => {
-          if (elem.name === vehicleName && !elem.count) {
-            elem.total_no -= 1
-            elem.count += 1
-            if (item.destination === destination) {
-              const distance = planetDistance / elem.speed
-              item.time = distance
-            }
-          }
-          if (elem.name !== vehicleName && elem.count) {
-            elem.total_no += 1
-            elem.count -= 1
-          }
-        })
-      }
+      if (item.destination >= destination) calcSpeedAndVehiclesLeft(item, vehicleName, destination, planetDistance)
       return item
     })
     onUpdateState(updatedState)
